e1632f3aacead198c12d6f347f7fd9c10500a32b
alexiessaenz <00400815@uca.edu.sv>
2024-02-27 09:14:38 -0600

    feat[vueelementsbss]: st v-proj counter

---
+++ 3. first vue app/03-fundamentos/src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>Counter</h2>
+  <p>{{ count }}</p>
+</template>
+
+<script>
+export default {
+    name: 'Counter',
+    data() {
+        return {
+            count: 0
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
