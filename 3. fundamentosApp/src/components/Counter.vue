95a6e8c53d20ed3175ecff8d5711aba5f8a5ba10
alexiessaenz <00400815@uca.edu.sv>
2024-03-01 15:54:55 -0600

    feat[vueelementsbss]: bs up to counterApp

---
+++ 3. fundamentosApp/src/components/Counter.vue
@@ -0,0 +1,43 @@
+<template>
+  <h2>Counter</h2>
+  <p>{{ count }} <sup>2</sup> = {{ count * count }}</p>
+  <p>{{ count }} <sup>2</sup> = {{ getSquaredMethod() }}</p>
+  <p>{{ count }} <sup>2</sup> = {{ getSquaredComputed }}</p>
+  <p>{{ count }} <sup>2</sup> = {{ getSquaredMethod() }}</p>
+  <p>{{ count }} <sup>2</sup> = {{ getSquaredComputed }}</p>
+  <p>{{ count }} <sup>2</sup> = {{ getSquaredMethod() }}</p>
+  <p>{{ count }} <sup>2</sup> = {{ getSquaredComputed }}</p>
+  <p>{{ count }} <sup>2</sup> = {{ getSquaredMethod() }}</p>
+  <p>{{ count }} <sup>2</sup> = {{ getSquaredComputed }}</p>
+  <p>{{ count }} <sup>2</sup> = {{ getSquaredMethod() }}</p>
+  <p>{{ count }} <sup>2</sup> = {{ getSquaredComputed }}</p>
+  <p>{{ count }} <sup>2</sup> = {{ getSquaredMethod() }}</p>
+</template>
+
+<script>
+export default {
+    name: 'Counter',
+    data() {
+        return {
+            count: 5
+        }
+    },
+    methods: {
+        getSquaredMethod() {
+            console.log('getSquaredMethod() is called')
+            return this.count * this.count
+        }
+    },
+    computed: {
+        getSquaredComputed() {
+            console.log('getSquaredComputed() is called')
+            return this.count * this.count
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
+
