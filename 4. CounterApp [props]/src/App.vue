b300000826b641305c9667dc961ce488b3cdf611
alexiessaenz <00400815@uca.edu.sv>
2024-03-09 21:06:41 -0600

    feat[vueelementsbss]: bs up to props by props

---
+++ 4. CounterApp [props]/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <!-- <Counter v-bind:start='15'/> -->
+  <Counter :start='10'/>
+  <Counter title="Entraron"/>
+  <Counter title="Salieron"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Counter
+  },
+  methods: {
+    sayHello() {
+      alert('Hello!')
+    }
+  },
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
